021e18384857b584c9e4e551cf2e50bd49832d3f
CloudyChris <damianfate@gmail.com>
2018-02-04 16:32:46 +0200

    form

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env electron
+const {app, BrowserWindow} = require('electron');
+app.on('ready', () => {
+	let win ;
+	win = new BrowserWindow({width:800, height:600, frame:false});
+	win.loadURL(`file://${__dirname}/index.html`);
+
+})
+
+app.on('window-all-closed', function() {
+      app.quit();
+ });
